ed502c740ff4c8f0658bd5c5145c4bc790c9da22
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2021-01-09 13:48:17 +0530

    improved user integrity

--- app/helpers/doubts_helper.rb
+++ app/helpers/doubts_helper.rb
@@ -1,2 +1,9 @@
 module DoubtsHelper
+    def doubt_author(doubt)
+        user_signed_in? && current_user.id == doubt.user_id
+    end
+
+    def comment_author(comment)
+        user_signed_in? && current_user.id == comment.user_id
+    end
 end
